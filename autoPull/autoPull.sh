62162757107840a585dfaa710e95f4163077732b
dd-han <han@dd-han.tw>
2016-03-25 10:53:22 +0800

    new script

---
+++ autoPull/autoPull.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+LIST='/home/dd-han/git/personal-scripts/autoPull/test.csv'
+
+totalLine=`wc -l test.csv | cut -d ' ' -f 1`
+
+for i in `seq 1 $totalLine`;do
+	line=`sed -n "${i},${i}p" "$LIST"`
+	if echo $line | grep -v '^#'> /dev/null;then
+		acc=`echo $line | cut -d ',' -f 1`
+		path=`echo $line | cut -d ',' -f 2`
+		cmd=`echo $line | cut -d ',' -f 3`
+
+		echo $acc $path $cmd
+		su ${acc} -c "cd ${path} && ${cmd} "
+		#su ${acc} -c "cd ${path} \&\& ${cmd}"
+	fi
+done
+
